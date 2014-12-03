293d37ff7ecb349e0009f297d9c5185f076da768
Gaël PORTAY <gael.portay@gmail.com>
2014-12-04 00:05:03 +0100

    Add support for flashing using sam-ba.
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 CROSS_COMPILE	?= arm-linux-gnueabi-
 BOARD		?= at91-sama5d3_xplained
+BOARDTYPE	?= $(shell echo $(BOARD) | sed -e 's,^at91-,at91,' -e 's,_.*$$,x-ek,')
+BOARDFAMILY	?= $(shell echo $(BOARD) | sed -e 's,_.*$$,,')
+BOARDSUFFIX	?= $(shell echo $(BOARD) | sed -e 's,^.*_,_,')
 
 board		:= $(shell echo $(BOARD) | sed -e 's,^at91-,at91,' -e '/sama[0-9]/s,^at91-*,,')
 DEFCONFIG	?= $(board)nf_linux_zimage_dt_defconfig
@@ -10,6 +13,8 @@ DTB		?= $(BOARD)
 
 MKFSUBIFSOPTS	?= --leb-size 0x1f000 --min-io-size 0x800 --max-leb-cnt 2048
 UBINIZEOPTS	?= --peb-size 0x20000 --min-io-size 0x800 --sub-page-size 0x800
+
+DEVICE		?= /dev/ttyACM0
 
 export CROSS_COMPILE
 
@@ -42,7 +47,7 @@ $(IMAGE): initramfs
 	ln -sf initramfs/$@
 
 kernel: $(IMAGE)
-	ln -sf $< $@
+	ln -sf initramfs/$< $@
 
 %.dtb:
 	@echo -e "\e[1mGenerating $@...\e[0m"
@@ -63,15 +68,35 @@ $(BOARD).ubi: ubi.ini kernel dtb persistant.ubifs
 	@echo -e "\e[1mGenerating $@...\e[0m"
 	ubinize $(UBINIZEOPTS) --output $@ $<
 
-ubi: $(BOARD).ubi
+$(BOARD)-mtd0.bin: at91bootstrap/binaries/at91bootstrap.bin
+
+$(BOARD)-mtd1.bin: $(BOARD).ubi
+
+bootstrap: $(BOARD)-mtd0.bin
+
+ubi: $(BOARD)-mtd1.bin
+
+$(BOARD)-nandflash4sam-ba.tcl: board-nandflash4sam-ba.tcl.in $(BOARD)-mtd0.bin $(BOARD)-mtd1.bin
+	sed -e "s,@BOOTSTRAPFILE@,$(BOARD)-mtd0.bin," \
+	    -e "s,@UBIFILE@,$(BOARD)-mtd1.bin," \
+	    -e "s,@BOARDFAMILY@,$(BOARDFAMILY)," \
+	    -e "s,@BOARDSUFFIX@,$(BOARDSUFFIX)," \
+	    $< >$@
+
+sam-ba: $(BOARD)-nandflash4sam-ba.tcl $(BOARD).ubi
+	@echo -e "\e[1mFlashing $@ $(DEVICE) using script $< ...\e[0m"
+	$@ $(DEVICE) $(BOARDTYPE) $<
+
+%.bin:
+	ln -sf $< $@
 
 clean:
 	make -C at91bootstrap clean
 	make -C initramfs clean
-	rm -f $(BOARD).ubi
+	rm -f $(BOARD).ubi $(BOARD)-mtd*.bin $(BOARD)-nandflash4sam-ba.tcl
 
 mrproper: clean
 	make -C at91bootstrap mrproper
 	make -C initramfs mrproper
-	rm -f $(IMAGE) kernel *.dtb dtb persistant.ubifs *.ubi
+	rm -f $(IMAGE) kernel *.dtb dtb persistant.ubifs *.ubi *.bin
 	rm -Rf persistant
---
+++ nandflash4sam-ba.tcl
@@ -0,0 +1,77 @@
+# Copyright (c) 2014 Gaël PORTAY <gael.portay@gmail.com>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the disclaimer below.
+#
+# Atmel's name may not be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+# DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------------
+
+################################################################################
+#  Main script: Load the Linux UBI RAMFS based System into NandFlash
+################################################################################
+
+################################################################################
+
+# check for proper variable initialization
+if {! [info exists boardFamily]} {
+   puts "-E- === Board family not defined ==="
+   exit
+}
+
+## Now check for the needed files
+if {! [file exists $bootstrapFile]} {
+   puts "-E- === AT91Bootstrap file not found ==="
+   exit
+}
+
+if {! [file exists $ubiFile]} {
+   puts "-E- === UBI file not found ==="
+   exit
+}
+
+set pmeccConfig 0xc0902405
+
+puts "-I- === Initialize the NAND access ==="
+NANDFLASH::Init
+
+if {$pmeccConfig != "none"} {
+   puts "-I- === Enable PMECC OS Parameters ==="
+   NANDFLASH::NandHeaderValue HEADER $pmeccConfig
+}
+
+puts "-I- === Erase all the NAND flash blocs and test the erasing ==="
+NANDFLASH::EraseAllNandFlash
+
+puts "-I- === Load the bootstrap in the first sector ==="
+if {$pmeccConfig != "none"} {
+   NANDFLASH::SendBootFilePmeccCmd $bootstrapFile
+} else {
+   NANDFLASH::sendBootFile $bootstrapFile
+}
+
+if {$pmeccConfig != "none"} {
+   puts "-I- === Enable trimffs ==="
+   NANDFLASH::NandSetTrimffs 1
+}
+
+puts "-I- === Load the UBI partition ==="
+send_file {NandFlash} "$ubiFile" 0x00020000 0
+
+puts "-I- === DONE. ==="
