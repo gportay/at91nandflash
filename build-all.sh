aaf9987f82b762bfcdc785ac61457a4c0d6903d6
Gaël PORTAY <gael.portay@gmail.com>
2015-05-28 16:20:39 +0200

    project: build-all.sh script.
    This script builds for all known targets.
    
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

---
+++ build-all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+run() {
+	board=$1
+	shift
+
+	case $board in
+	*)
+		make BOARD=$board $*
+		;;
+	esac >/dev/null
+	echo "$board: done"
+}
+
+for board in at91-sam9260ek at91-sam9261ek at91-sam9263ek at91-sam9m10g45ek at91-sam9n12ek at91-sam9rlek at91-sam9g20ek at91-sam9g20ek_2mmc at91-sam9g15ek at91-sam9g25ek at91-sam9g35ek at91-sam9x25ek at91-sam9x35ek at91-sama5d31ek at91-sama5d33ek at91-sama5d34ek at91-sama5d35ek at91-sama5d36ek at91-sama5d3x_cmp at91-sama5d3_xplained at91-sama5d4ek at91-sama5d4_xplained; do
+	run $board $*
+done
